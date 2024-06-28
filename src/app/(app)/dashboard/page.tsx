5e0c52e02443c03160a9de724c1fdaab6f08884f
jaskrrish <jashehz@gmail.com>
2024-06-28 19:37:33 +0530

    Refactor key prop in UserDashboard component

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -182,7 +182,7 @@ function UserDashboard() {
         {messages.length > 0 ? (
           messages.map((message, index) => (
             <MessageCard
-              key={message._id}
+              key={message._id as string}
               message={message}
               onMessageDelete={handleDeleteMessage}
             />
